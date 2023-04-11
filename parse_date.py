0ee208ee6c2a8a53fb81857cf2597b2b2aa1d8ea
oronar1 <oronara@gmail.com>
2023-04-11 10:43:02 +0300

    Update parse_date.py

--- parse_date.py
+++ parse_date.py
@@ -2,11 +2,12 @@ import re
 
 #define parse_date below
 def parse_date(data):
-    date_regex=re.compile(r'^(\d{2}|\d{1})(,|.|\/)(\d{2})(,|.|\/)(\d{4})$')
+    date_regex=re.compile(r'^(\d{2}|\d{1})[,/.](\d{2})[,/.](\d{4})$')
+               #re.compile(r'^(\d{2}|\d{1})(,|/|.)(\d{2})(,|/|.)(\d{4})$')
     matches=date_regex.search(data)
     if matches:
         return {'d':matches.group(1),
-                'm':matches.group(3),
-                'y':matches.group(5)
+                'm':matches.group(2),
+                'y':matches.group(3)
         }
     return None
