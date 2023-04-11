6f68e3a3b691e3198179307395cbb70dfd7399c0
oronar1 <oronara@gmail.com>
2023-04-11 10:39:34 +0300

    Create parse_date.py
    Date Parsing 
    
    Function called parse_date  that accepts a single string.  Code should check to see if the string matches a date format.  Using the DMY format of "dd/mm/yyyy", but it should also work with "dd.mm.yyyy" and "dd,mm,yyyy". If you are American, note that Day if before Month!  However, rather than simply returning True or False if the string matches..instead return a dictionary containing the three parts of the date with the keys "d" , "m" , and "y"  like so:
    
        parse_date("01/22/1999") # {'d': '01', 'm': '22', 'y': '1999'}
    
     Note: the string should be an EXACT match, containing the date and nothing else. If there is no match, return None
    
        parse_date("12,04,2003")  #{'d': '12', 'm': '04', 'y': '2003'}
        parse_date("12.11.2003")  #{'d': '12', 'm': '11', 'y': '2003'}
        parse_date("12.11.200312") #None

---
+++ parse_date.py
@@ -0,0 +1,12 @@
+import re
+
+#define parse_date below
+def parse_date(data):
+    date_regex=re.compile(r'^(\d{2}|\d{1})(,|.|\/)(\d{2})(,|.|\/)(\d{4})$')
+    matches=date_regex.search(data)
+    if matches:
+        return {'d':matches.group(1),
+                'm':matches.group(3),
+                'y':matches.group(5)
+        }
+    return None
