4ee4c42549e3598005970c619aae7709586eaf4f
oronar1 <oronara@gmail.com>
2023-04-26 12:59:19 +0300

    Create find_the_duplicate2.py
    Function called find_the_duplicate which accepts an array of numbers containing a single duplicate. The function returns the number which is a duplicate or None if there are no duplicates.

---
+++ find_the_duplicate2.py
@@ -0,0 +1,12 @@
+'''
+find_the_duplicate([1,2,1,4,3,12]) # 1
+find_the_duplicate([6,1,9,5,3,4,9]) # 9
+find_the_duplicate([2,1,3,4]) # None
+'''
+
+def find_the_duplicate(arr):
+    arr.sort()
+    for i in range(0,len(arr)-1):
+        if arr[i] == arr[(i+1)]:
+            return arr[i]    
+    return None
