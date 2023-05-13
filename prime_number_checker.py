969f727691c5f659196ee87ac245c54fcf4438d4
oronar1 <oronara@gmail.com>
2023-05-13 14:57:15 +0300

    Update prime_number_checker.py

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -8,8 +8,5 @@ def prime_checker(number):
     else:
         print("It's not a prime number.")
 
-#Write your code above this line 👆
-    
-#Do NOT change any of the code below👇
 n = int(input("Check this number: "))
 prime_checker(number=n)
