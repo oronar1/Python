7da2c1364a90b45c39d025343fe545e4680536ff
oronar1 <oronara@gmail.com>
2023-06-18 18:17:15 +0300

    main.py

--- pong/main.py
+++ pong/main.py
@@ -16,10 +16,10 @@ ball = Ball()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
