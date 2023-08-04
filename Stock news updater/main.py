6efeeb6b4f2d3f04b3ceab26bd0e6975e943023a
oronar1 <oronara@gmail.com>
2023-08-04 13:33:11 +0300

    Update main.py

--- Stock news updater/main.py
+++ Stock news updater/main.py
@@ -66,15 +66,4 @@ if abs(diff_percent) > 5:
             from_="+12187789366",
             to= '**********'
         )
-    #Optional TODO: Format the message like this:
-
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
