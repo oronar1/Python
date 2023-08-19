e19924fe512c4502ab302d55fee37de58675ef3a
oronar1 <oronara@gmail.com>
2023-08-19 12:49:37 +0300

    Update notification_manager.py

--- Flight club/notification_manager.py
+++ Flight club/notification_manager.py
@@ -1,13 +1,13 @@
 import smtplib
 from twilio.rest import Client
 
-TWILIO_SID = "AC26a822665bb0b9a76af184fb044e843e"
-TWILIO_AUTH_TOKEN = "ffd97e89489d5d8ee7f7497d40a22e3b"
-TWILIO_VIRTUAL_NUMBER = "+12187789366"
-TWILIO_VERIFIED_NUMBER = '+972545310514'
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
+TWILIO_VIRTUAL_NUMBER = ""
+TWILIO_VERIFIED_NUMBER = ''
 MAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com" #YOUR EMAIL PROVIDER SMTP ADDRESS "smtp.gmail.com"
-MY_EMAIL = "zeevsemen@gmail.com"
-MY_PASSWORD = "kjpxcvbhcxkbused"#Stqr%$dsaf^adgbdcf"
+MY_EMAIL = ""
+MY_PASSWORD = ""
 
 class NotificationManager:
 
